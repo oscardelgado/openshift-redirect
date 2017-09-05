364acc091b06e6219dc2d0f19f25a75b012d9304
Óscar Delgado Santamaría <oscardelgado83@gmail.com>
2017-09-05 17:07:24 +0200

    Update DayResource.java

--- src/main/java/com/easymenuplanner/backend/rest/resources/DayResource.java
+++ src/main/java/com/easymenuplanner/backend/rest/resources/DayResource.java
@@ -38,8 +38,8 @@ public class DayResource extends BackendResource {
     protected HttpServletRequest servletRequest;
     
     private String getFullURL() {
-        logger.info("localName: " + servletRequest.getLocalName());
-        String requestURL = servletRequest.getRequestURL().toString().replaceAll(servletRequest.getLocalName(), "easymenuplanerwildfly-ods.rhcloud.com");
+        logger.info("serverName: " + servletRequest.getServerName());
+        String requestURL = servletRequest.getRequestURL().toString().replaceAll(servletRequest.getServerName(), "easymenuplanerwildfly-ods.rhcloud.com");
         if (servletRequest.getQueryString() != null) {
             requestURL.concat("?").concat(servletRequest.getQueryString());
         }
